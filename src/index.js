e867e8b498cbf294dfd080b94f8037a681f7ed30
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-05 16:58:07 +0530

    css fix for 2x2 grid

--- src/index.js
+++ src/index.js
@@ -13,9 +13,10 @@ class Blog extends React.Component{
     return (
       <div>
         <h1>{name}</h1>
-        {
-          blogItems.map(item => (<BlogItems category={item.category} color={item.color} />))
-        }
+        <div class="blog">
+        {blogItems.map(item => (<BlogItems category={item.category} color={item.color} />))}
+        </div>
+          
       </div>
     )
   }
@@ -27,15 +28,11 @@ class BlogItems extends React.Component{
       color,
     } = this.props;
     return (
-        <div>
-        <Grid columns={2}>
-        <Grid.Row>
+        <div class="blogItems">
         <Label as='a' color={color}>{category}</Label>
         <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
         </p>
-        </Grid.Row>
-        </Grid>
       </div>
       )
     }
