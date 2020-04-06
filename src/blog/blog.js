b61e7f0af28774458a06b166746a8f829af25c15
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-06 13:44:31 +0530

    blog in separate folder

---
+++ src/blog/blog.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import BlogItems from './blogItem';
+
+const blogItems = [{ category: "news", color: "red", id: 1 }, { category: "info", color: "grey", id: 2 }, { category: "tech", color: "blue", id: 3 }, { category: "x-factor", color: "green", id: 4 }]
+class Blog extends React.Component{
+  render() {
+    const name = "My Super Blog";
+    return (
+      <div>
+        <h1>{name}</h1>
+        <div class="blog">
+          {blogItems.map(item => (<BlogItems category={item.category} color={item.color} id= {item.id} />))}
+        </div>
+          
+      </div>
+    )
+  }
+}
+
+export default Blog;
