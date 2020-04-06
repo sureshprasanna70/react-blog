ca13848c587694e69f76c1e235ca339eefa799cd
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-06 13:44:43 +0530

    game in separate folder

---
+++ src/game/game.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Board from './board';
+
+class Game extends React.Component{
+    render() {
+      return (
+        
+        <div class="game">
+          <div class="game-board">
+            <Board/>
+          </div> 
+          <div class="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+          </div>
+          
+      )
+    }
+}
+export default Game;
