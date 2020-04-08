0138af266359cbe1f8676b5fa5487ba08416f93f
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-08 21:05:41 +0530

    src/index.js: includes link in navbar for shop

--- src/index.js
+++ src/index.js
@@ -1,38 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import 'semantic-ui-css/semantic.min.css'
 
+import './index.css';
+import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css'
+import App from './App';
 import Blog from './blog/blog';
 import Game from './game/game';
+import Shop from './shop/shop'
+import ProductDetails from './shop/products/ProductDetails'
+import Cart from './shop/Cart'
+import Default from './Default'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Container } from 'semantic-ui-react'
+import { Container, Icon } from 'semantic-ui-react'
 import * as serviceWorker from './serviceWorker';
 
 class Home extends React.Component{
   render() {
     return (
       <div>
+        <img src={logo} alt="logo" />
          <Router>
           <div>
-            <div class="ui menu">
+            <div className="ui menu">
+              <Link className="item" to="/"><Icon name='home' color="orange" /></Link>
               <Link className="item" to="/blog">Blog</Link>
               <Link className="item" to="/game">Game</Link>
+              <Link className="item" to="/shop">Shop</Link>
+              <div className="right menu">
+                <Link className="item" to="/cart"><Icon name="shopping cart" />Cart</Link>
+              </div>
             </div>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/blog">
-            <Blog />
-          </Route>
-          <Route path="/game">
-            <Game />
-          </Route>
-        </Switch>
-      </div>
+            <Switch>
+              <Route exact path="/">
+                <App />
+              </Route>
+              <Route path="/blog">
+                <Blog />
+              </Route>
+              <Route path="/game">
+                <Game />
+              </Route>
+              <Route path="/shop/">
+                <Shop />
+              </Route>
+              <Route path="/details" >
+                <ProductDetails/>
+              </Route>
+              <Route path="/cart" >
+                <Cart/>
+              </Route>
+              <Route component={Default} />
+            </Switch>
+          </div>
         </Router>
-        </div>
+      </div>
     )
   }
 }
