ad76c875210304bd5626291774ccb2c578f14cd6
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-08 20:58:18 +0530

    src/shop/products: skeleton for products in shop

---
+++ src/shop/products/Product.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
+
+export default class Product extends Component{
+    render() {
+        return (
+           <Header>Product</Header>
+        )
+    }
+}
---
+++ src/shop/products/ProductDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ProductDetails extends Component{
+    render() {
+        return (
+            <div>
+              <h4>ProductDetails</h4>  
+            </div>
+        )
+    }
+}
