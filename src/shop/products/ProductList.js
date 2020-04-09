11ffe3a1c681156f69ca1a5d110582b3a4695081
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-09 15:17:55 +0530

    src/shop/products/ProductList.js: reads data from ProductConsumer context

--- src/shop/products/ProductList.js
+++ src/shop/products/ProductList.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import { Container, Grid } from 'semantic-ui-react';
+import { ProductConsumer } from '../../context';
 export default class ProductList extends Component{
+ 
     render() {
         return (
-            <div>
-                <Product/>
-            </div>
+            <React.Fragment>
+                    <Container>
+                        <Grid className="mt-2">
+                            <ProductConsumer>
+                                {(value) => {
+                                    return value.products.map(product => {
+                                        return <Product key={product.id} product={product}/>
+                                    })
+                                }}
+                            </ProductConsumer>
+                        </Grid>
+                    </Container>
+            </React.Fragment>
         )
     }
 }
