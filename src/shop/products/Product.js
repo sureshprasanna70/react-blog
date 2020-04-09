3d82f6af60ead8af483a0fa0501288a16cdd29c6
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-09 15:19:51 +0530

    products/Product.js: displays indivdual product tiles

--- src/shop/products/Product.js
+++ src/shop/products/Product.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Grid, Card, Image } from 'semantic-ui-react';
 
 export default class Product extends Component{
     render() {
+    const {title, img, price, company} = this.props.product
+
         return (
-           <Header>Product</Header>
+            <div onClick={() => console.log("you clicked me!!")}>
+                <Grid.Column>
+                    <Card>
+                        <Link to="/details">
+                            <Image src={img}/>
+                        </Link>
+                        <Card.Content>
+                            <Card.Header>
+                                {title}
+                            </Card.Header>
+                            <Card.Description>
+                                ${price}
+                            </Card.Description>
+                            <Card.Meta>
+                                {company}
+                            </Card.Meta>
+                        </Card.Content>
+                    </Card>
+                </Grid.Column>
+            </div>
         )
     }
 }
