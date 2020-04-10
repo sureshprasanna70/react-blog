271fc5ba69e7591b6486563cd28eadec9ff75bb4
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-10 12:43:19 +0530

    src/shop/products/Product.js: adds a boolean check for inCart

--- src/shop/products/Product.js
+++ src/shop/products/Product.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Card, Image } from 'semantic-ui-react';
+import { Grid, Card, Image, Button, Icon } from 'semantic-ui-react';
 
 export default class Product extends Component{
+    truncate(str) {
+        return str.length > 50 ? str.substring(0, 50) + "..." : str;
+    }
     render() {
-    const {title, img, price, company} = this.props.product
+    const {title, img, price,  info, inCart} = this.props.product
 
         return (
             <div onClick={() => console.log("you clicked me!!")}>
@@ -18,11 +21,18 @@ export default class Product extends Component{
                                 {title}
                             </Card.Header>
                             <Card.Description>
-                                ${price}
-                            </Card.Description>
+                                {this.truncate(info)}
                             <Card.Meta>
-                                {company}
-                            </Card.Meta>
+                                <Button basic size="large" animated='vertical' color="blue" disabled={inCart ? true: false} onClick={() => console.log("added to cart")}>
+                                    <Button.Content visible>
+                                        <Icon name='shop' /> {inCart ? "Already in Cart" : "$"+price}
+                                    </Button.Content>
+                                    <Button.Content hidden>
+                                        Add to Cart
+                                    </Button.Content>
+                                </Button>
+                                </Card.Meta>
+                                </Card.Description>
                         </Card.Content>
                     </Card>
                 </Grid.Column>
