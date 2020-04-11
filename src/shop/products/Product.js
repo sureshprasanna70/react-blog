fdd15d7fef7a927159fbc69a0f6bdce8676612dc
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-11 12:55:24 +0530

    src/shop/products/Product.js: invokes handledetail to show specific product

--- src/shop/products/Product.js
+++ src/shop/products/Product.js
@@ -2,41 +2,46 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Grid, Card, Image, Button, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import { ProductConsumer } from '../../context';
 
 export default class Product extends Component{
     truncate(str) {
         return str.length > 50 ? str.substring(0, 50) + "..." : str;
     }
     render() {
-    const {title, img, price,  info, inCart} = this.props.product
+    const {id, title, img, price,  info, inCart} = this.props.product
 
         return (
             <Grid.Column>
-                <div onClick={() => console.log("you clicked me!!")}>
-                    <Card>
-                        <Link to="/details">
-                            <Image src={img}/>
-                        </Link>
-                        <Card.Content>
-                            <Card.Header>
-                                {title}
-                            </Card.Header>
-                            <Card.Description>
-                               <p>{this.truncate(info)}</p>
-                            <Card.Meta>
-                                <Button basic size="large" animated='vertical' color="blue" disabled={inCart ? true: false} onClick={() => console.log("added to cart")}>
-                                    <Button.Content visible>
-                                        <Icon name='shop' /> {inCart ? "Already in Cart" : "$"+price}
-                                    </Button.Content>
-                                    <Button.Content hidden>
-                                        Add to Cart
-                                    </Button.Content>
-                                </Button>
-                                </Card.Meta>
-                                </Card.Description>
-                        </Card.Content>
-                    </Card>
-                </div>
+                <ProductConsumer>
+                    {value => (
+                        <div onClick={() => value.handleDetail(id)}>
+                        <Card>
+                            <Link to="/details">
+                                <Image src={img}/>
+                            </Link>
+                            <Card.Content>
+                                <Card.Header>
+                                    {title}
+                                </Card.Header>
+                                <Card.Description>
+                                   <p>{this.truncate(info)}</p>
+                                <Card.Meta>
+                                    <Button basic size="large" animated='vertical' color="blue" disabled={inCart ? true: false} onClick={() => console.log("added to cart")}>
+                                        <Button.Content visible>
+                                            <Icon name='shop' /> {inCart ? "Already in Cart" : "$"+price}
+                                        </Button.Content>
+                                        <Button.Content hidden>
+                                            Add to Cart
+                                        </Button.Content>
+                                    </Button>
+                                    </Card.Meta>
+                                    </Card.Description>
+                            </Card.Content>
+                        </Card>
+                        </div>
+                    )}
+                </ProductConsumer>
             </Grid.Column>
         )
     }
