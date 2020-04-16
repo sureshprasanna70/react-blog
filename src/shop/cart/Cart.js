faba73274bcd128f2675310078b2e519370d8a13
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-16 13:14:00 +0530

    Cart.js: display cart components

--- src/shop/cart/Cart.js
+++ src/shop/cart/Cart.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
 import { ProductConsumer } from '../../context';
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
+import CartList from './CartList';
+import CartTotal from './CartTotal';
 export default class Cart extends Component{
     render() {
         return (
-                <ProductConsumer>
-                {(value) => (
-                    <div className="mt-1">    
-                        {value.getCartCount() > 0 ? <CartColumns/> : <EmptyCart/>}
-                        
-                    </div>
-                    )}
+            <div className="mt-1">
+                 <ProductConsumer>
+                    {(value) => {
+                        if(value.getCartCount() > 0){
+                            return <div>
+                                <CartColumns /><CartList value={value} />
+                                <CartTotal value={value}/>
+                            </div>
+                        } else {
+                            return <EmptyCart/>
+                        }
+                    }}
                 </ProductConsumer>
+            </div>
         )
     }
 }
