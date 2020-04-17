4b0750b5db7e5b99f10128b6860162562d809f5d
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-17 20:07:37 +0530

    js/reducers/index.js: adds dummy data to initial state

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,11 @@
 import { ADD_ARTICLE } from '../constants/action-types';
+import Faker from 'faker';
 
 const initialState = {
-    articles: []
+  articles: [{ id: 1, title: Faker.lorem.sentence(), content: Faker.lorem.paragraphs(2), color: "red", category: Faker.lorem.word() },
+    { id: 2, title: Faker.lorem.sentence(), content: Faker.lorem.paragraphs(2), color: "yellow", category: Faker.lorem.word() },
+    { id: 3, title: Faker.lorem.sentence(), content: Faker.lorem.paragraphs(2), color: "green", category: Faker.lorem.word() },
+    { id: 4, title: Faker.lorem.sentence(), content: Faker.lorem.paragraphs(2), color: "blue", category: Faker.lorem.word() }]
   };
   
 function rootReducer(state = initialState, action) {
@@ -10,8 +14,8 @@ function rootReducer(state = initialState, action) {
             articles: state.articles.concat(action.payload)
           });
       
-      }
+    }
     return state;
-  };
+};
   
   export default rootReducer;
