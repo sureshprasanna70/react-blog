135a00ccf996ed95af6933d09b183ee7ced3d4aa
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-18 18:28:46 +0530

    blog/blog.js: import blogForm to render new article form

--- src/blog/blog.js
+++ src/blog/blog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BlogItems from './blogItem';
 import { connect } from 'react-redux';
-
+import BlogForm  from './blogForm';
 
 const mapStateToProps = state => {
   return {articles: state.articles};
@@ -10,6 +10,9 @@ function BlogList({articles}){
     return (
       <div className="mt-1">
         <h1 className="shop-title">Redux V</h1>
+        <div class="right floated">
+          <BlogForm />
+        </div>
         <div className="blog">
           {articles.map(article => (
             <BlogItems key={article.id} title={article.title}  content={article.content} color={article.color} category={article.category}/>
