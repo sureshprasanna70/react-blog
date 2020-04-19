113cda1409703eed55a34e5c96ec1d42ff0cd7c6
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-19 18:57:48 +0530

    src/blog/blog.js: invoke getData to fetch remote data

--- src/blog/blog.js
+++ src/blog/blog.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import BlogItems from './blogItem';
 import { connect } from 'react-redux';
-import BlogForm  from './blogForm';
+import BlogForm from './blogForm';
+import { getData } from "../js/actions/index";
 
 const mapStateToProps = state => {
-  return {articles: state.articles};
+  return {articles: state.articles.slice(0,10)};
 }
-function BlogList({articles}){
+
+class BlogList extends Component{
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    this.props.getData()
+  }
+  render() {
+    const { articles } = this.props;
     return (
       <div className="mt-1">
         <h1 className="shop-title">Redux V</h1>
-        <div class="right floated">
+        <div className="right floated">
           <BlogForm />
         </div>
         <div className="blog">
           {articles.map(article => (
-            <BlogItems key={article.id} title={article.title}  content={article.content} color={article.color} category={article.category}/>
+            <BlogItems key={article.id} title={article.title}  body={article.body} color={article.color} category={article.category}/>
           ))}
         </div>
       </div>
     )
   }
+    
+  }
 
-const Blog = connect(mapStateToProps)(BlogList);
+const Blog = connect(mapStateToProps,{getData})(BlogList);
 export default Blog;
