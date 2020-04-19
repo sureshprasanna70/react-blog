b21d7bdbbb0c9c69c9eb9133d5c3d624b0b026e9
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-19 18:40:57 +0530

    js/store/index.js: loads thunk middleware

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
-import rootReducer from "../reducers/index";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+ import rootReducer from "../reducers/index";
 
-const store = createStore(rootReducer);
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, storeEnhancers(applyMiddleware(thunk)));
 
 export default store;
